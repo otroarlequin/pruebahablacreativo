2e4bb8a0d9c4c6c053623f99a196ca8a1e9a8036
otroarlequin <38340299+otroarlequin@users.noreply.github.com>
2018-04-12 23:45:21 -0500

    Carga inicial de archivos

---
+++ index.php
@@ -0,0 +1,220 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" href=".\css\bootstrap.css" crossorigin="anonymous">
+	<link rel="stylesheet" href=".\css\magnific-popup.css">
+	<link rel="stylesheet" href=".\css\style.css">
+
+	<script type="text/javascript" src=".\js\jquery-3.3.1.min.js"></script>
+	<script type="text/javascript" src=".\js\popper.min.js"></script>
+	<script type="text/javascript" src=".\js\bootstrap.js"></script>
+
+	<script type="text/javascript" src=".\js\bootstrap.bundle.js"></script>
+	<script type="text/javascript" src=".\js\bootstrap.bundle.min.js"></script>
+
+	<script type="text/javascript" src=".\js\magnificpopup.js"></script>
+
+	<meta https-equiv="Content-Type" content="text/html; charset=utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+</head>
+<body>
+	<header>
+		<nav class="navbar navbar-expand-lg bg-nav" id="bg-nav">
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample08" aria-controls="navbarsExample08" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<img src="resources/Remanso-05.png" class="logo"/>
+			<div class="collapse navbar-collapse justify-content-md-center" id="navbarsExample08">
+				<ul class="navbar-nav">
+					<li class="nav-item active">
+						<a class="nav-link" href="#">Proyecto</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Galeria</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Apartamentos</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Mapa</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Contacto</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+	</header>
+	<section id="home">
+		<div class="container">
+			<div class="row justify-content-md-center" id="principal">
+				<div class="col-sm-6 col-xs-12" id="bono"></div>
+				<div class="col-sm-6 col-xs-12" id="formulario">
+					<div class="row justify-content-md-center">
+						<div class="col-sm-12">
+							<form>
+								<table cellpadding="3">
+									<tr>
+										<td><input type="text" class="form-control" placeholder="Nombre*"></td>
+									</tr>
+									<tr>
+										<td><input type="text" class="form-control" placeholder="Correo Electronico*"></td>
+									</tr>
+									<tr>
+										<td><input type="text" class="form-control" placeholder="Telefono*"></td>
+									</tr>
+									<tr>
+										<td><input type="text" class="form-control" placeholder="Ciudad*"></td>
+									</tr>
+									<tr>
+										<td><textarea rows="2" class="form-control" placeholder="Comentario*"></textarea></td>
+									</tr>
+									<tr>
+										<td>
+											<span>Al hacer clic en enviar, estas aceptando los <b>Terminos y condiciones</b></span>
+										</td>
+									</tr>
+									<tr>
+										<td style="text-align: center;">
+											<button type="button" class="btn">Enviar</button>
+										</td>
+									</tr>
+								</table>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section id="apartamentos">
+		<div class="container">
+			<figure>
+				<img src="resources/Remanso-19.png" class="d-none d-md-block"/>
+				<img src="resources/Remanso-20.png" class="d-md-none" />
+				<figcaption></figcaption>
+			</figure>
+			<div class="row">
+				<div class="col-sm-4">
+					<a class="image-popup-vertical-fit" href="resources/Remanso-21.png">
+						<img src="resources/Remanso-21.png">
+					</a>
+				</div>
+				<div class="col-sm-4">
+					<a class="image-popup-vertical-fit" href="resources/Remanso-22.png">
+						<img src="resources/Remanso-22.png">
+					</a>
+				</div>
+				<div class="col-sm-4">
+					<a class="image-popup-vertical-fit" href="resources/Remanso-23.png">
+						<img src="resources/Remanso-23.png">
+					</a>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-sm-4">
+					<a class="image-popup-vertical-fit" href="resources/Remanso-24.png">
+						<img src="resources/Remanso-24.png">
+					</a>
+				</div>
+				<div class="col-sm-4">
+					<a class="image-popup-vertical-fit" href="resources/Remanso-25.png">
+						<img src="resources/Remanso-25.png">
+					</a>
+				</div>
+				<div class="col-sm-4">
+					<a class="image-popup-vertical-fit" href="resources/Remanso-26png">
+						<img src="resources/Remanso-26.png">
+					</a>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section id="footer">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-12">
+					<h1 class="text-uppercase">sala de negocios</h1>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-sm-3" class="text-center">
+					<img src="resources/Remanso-31.png"/>
+					<p class="font-weight-bold">
+						Tel:
+						<br>
+						3439241 - 3137050212
+					</p>
+				</div>
+				<div class="col-sm-3">
+					<img src="resources/Remanso-32.png"/>
+					<p class="font-weight-bold">
+						Dirección:<br/>
+						Calle 6 sur # 79 - 158 Medellín
+					</p>
+				</div>
+				<div class="col-sm-3">
+					<img src="resources/Remanso-33.png"/>
+					<p class="font-weight-bold">Hora de Atención:
+						<br/>
+						10:00 am - 5:30pm
+					</p>
+				</div>
+				<div class="col-sm-3">
+					<img src="resources/Remanso-34.png"/>
+					<p class="font-weight-bold">
+						Correo:<br/>
+						<span class="font-weight-normal">remansodelrodeo@une.net.co</span>
+					</p>
+				</div>
+			</div>
+			<div class="row" style="padding-top: 20px;">
+				<p>Las imágenes utilizadas sons representaciones digitales del diseño y junto con las oficinas, locales y apartamentos pueden variar en la construcción final. El mobiliario, electrodomésticos, gasodomésticos, acabados y demás elementos que aparecen en las imagenes son una representación a fin de ilustrar la utilización de los espacios y pueden diferir de lo entregado a no ser entregado según lo convenido en los contratos.Las áreas privadas y construidas podrán sufrir ajustes y quedarán finalmente determinadas en los contratos suscritos por las partes</p>
+			</div>
+			<div class="row" style="padding-top: 20px;">
+				<img src="resources/Remanso-35.png"/>
+			</div>
+		</div>
+	</section>
+
+	<script>
+		$(document).ready(function() {
+
+			$('.image-popup-vertical-fit').magnificPopup({
+				type: 'image',
+				closeOnContentClick: true,
+				mainClass: 'mfp-img-mobile',
+				image: {
+					verticalFit: true
+				}
+
+			});
+
+			$('.image-popup-fit-width').magnificPopup({
+				type: 'image',
+				closeOnContentClick: true,
+				image: {
+					verticalFit: false
+				}
+			});
+
+			$('.image-popup-no-margins').magnificPopup({
+				type: 'image',
+				closeOnContentClick: true,
+				closeBtnInside: false,
+				fixedContentPos: true,
+		mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+		image: {
+			verticalFit: true
+		},
+		zoom: {
+			enabled: true,
+			duration: 300 // don't foget to change the duration also in CSS
+		}
+	});
+
+		});
+	</script>
+</body>
+</html>
